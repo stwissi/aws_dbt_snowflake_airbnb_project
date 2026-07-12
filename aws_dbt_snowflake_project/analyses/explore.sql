fe487533416ebe54d7031847ca8a8e18a1f32b20
Sofian <sofiantwissi0@gmail.com>
2026-07-12 16:11:13 +0300

    chore: cleanup repository and remove unnecessary files

--- aws_dbt_snowflake_project/analyses/explore.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM {{ref("bronze_bookings")}}
--- aws_dbt_snowflake_project/analyses/playing.sql
+++
@@ -1,9 +0,0 @@
--- This query shows you exactly what new data is sitting in the source table 
--- that has NOT YET been loaded into your bronze table.
-
-SELECT * 
-FROM {{ source('staging', 'bookings') }}
-WHERE created_at > (
-    SELECT COALESCE(MAX(created_at), '1900-01-01') 
-    FROM {{ ref('bronze_bookings') }}
-)
